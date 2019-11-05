fb0962ce8bf6ec94a98aefdbd0ff4b37e9166947
thlambert <43218422+thlambert@users.noreply.github.com>
2019-11-04 22:56:49 -0600

    Add files via upload

---
+++ Prog1/Prog5/CreateUser.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateUser.aspx.cs" Inherits="Prog1.Prog5.CreateUser" %>
+
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="../StyleSheetProg3.css" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CS 3870 - Prog5</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="CS3870Title">Web Protocols, Technologies and Applications </h1>
+            <h2 class="CS3870Name">Thomas Lambert</h2>
+        </div>
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Sign Up for your New Account"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="ID"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="UserName:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtUN" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtUN" ErrorMessage="Username is required!" ForeColor="Red"></asp:RequiredFieldValidator>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Password:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPW" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtPW" ErrorMessage="Password is required!" ForeColor="Red"></asp:RequiredFieldValidator>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" Text="Confirm Password:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPWConfirm" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtPWConfirm" ErrorMessage="Confirm Password is required!" ForeColor="Red"></asp:RequiredFieldValidator>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>
+                        <asp:Button ID="btnCreate" runat="server" OnClick="btnCreate_Click" Text="Create User" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Prog1/Prog5/SQLDataClass.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Prog1.Prog5
+{
+    public class SQLDataClass
+    {
+        private const string ConStr = "Data Source=Alpha;" +
+            "Initial Catalog = UWPCS3870; Persist Security Info=True;" +
+            "User ID = MSCS; Password=MasterInCS";
+        private static System.Data.SqlClient.SqlDataAdapter prodAdapter;
+        private static System.Data.SqlClient.SqlCommand prodCmd = new System.Data.SqlClient.SqlCommand();
+        private static System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection();
+        private static  String ud;
+        public static System.Data.DataTable tblProduct = new System.Data.DataTable("Product");
+
+        public static void setud(String Newud)
+        {
+            ud = Newud;
+        }
+        public static String getud()
+        {
+            return ud;
+        }
+        public static void setupProdAdapter()
+        {
+            con.ConnectionString = ConStr;
+            prodCmd.Connection = con;
+            prodCmd.CommandType = System.Data.CommandType.Text;
+            prodCmd.CommandText = "Select * from Product order by ProductID";
+            prodAdapter = new System.Data.SqlClient.SqlDataAdapter(prodCmd);
+            prodAdapter.FillSchema(tblProduct, System.Data.SchemaType.Source);
+        }
+
+        public void Session_Start()
+        {
+
+        }
+
+        public static void getAllProducts()
+        {
+            if (prodAdapter == null)
+            {
+                setupProdAdapter();
+            }
+            prodCmd.CommandText = "Select * from Product order by ProductID";
+            try
+            {
+                if (!(tblProduct == null))
+                    tblProduct.Clear();
+                prodAdapter.Fill(tblProduct);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            SQLDataClass.setupProdAdapter();
+        }
+
+        public static void UpdateProduct(string theID, string newName, double newPrice, string newDesc)
+        {
+            prodCmd.CommandText = "Update Product "
+                + "Set ProductName = '" + newName + "', "
+                + "UnitPrice = " + newPrice + ", "
+                + "Description = '" + newDesc + "' "
+                + "Where ProductID = '" + theID + "'";
+            try
+            {
+                con.Open();
+                prodCmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void DeleteProduct(string theID)
+        {
+            prodCmd.CommandText = "Delete From Product "
+                + "Where ProductID = '" + theID + "'";
+            try
+            {
+                con.Open();
+                prodCmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static bool AddProduct(string ID, string Name, int Price, string Desc)
+        {
+            try
+            {
+                con.Open();
+                string checkuser = "select count(*) from Product where ProductID = '" + ID + "'";
+                SqlCommand com1 = new SqlCommand(checkuser, con);
+                int temp = Convert.ToInt32(com1.ExecuteScalar().ToString());
+                if (temp > 0)
+                {
+                    con.Close();
+                    return false;
+                }
+                else
+                {
+                    string insertQuery = "INSERT INTO Product(ProductID, ProductName, UnitPrice, Description)" +
+                    " values (@keyID, @Uname, @Price, @Desc)";
+                    SqlCommand com = new SqlCommand(insertQuery, con);
+                    com.Parameters.AddWithValue("@keyID", ID);
+                    com.Parameters.AddWithValue("@Uname", Name);
+                    com.Parameters.AddWithValue("@Price", Price);
+                    com.Parameters.AddWithValue("@Desc", Desc);
+                    try
+                    {
+                        com.ExecuteNonQuery();
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+
+        public static bool checkID(string ID)
+        {
+            try
+            {
+                con.Open();
+                string checkuser = "select count(*) from Product where ProductID = '" + ID + "'";
+                SqlCommand com1 = new SqlCommand(checkuser, con);
+                int temp = Convert.ToInt32(com1.ExecuteScalar().ToString());
+                if (temp > 0)
+                {
+                    con.Close();
+                    return true;
+                }
+                else
+                {
+                    con.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+            
+    }
+
+}
